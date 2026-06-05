3781d3d2a89240c8ea5d9ee5168857bf7878884d
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-06-05 15:55:16 +0200

    Updated the client with saver dataTyping for responses

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,9 @@
 // here is the most important component that connect both side
-
 export const API_URL = "http://127.0.0.1:8000/"; // change this to your backend url if it's different
+import type { UploadResponse } from "../interfaces/upload";
+import type { AskResponse } from "../interfaces/ask";
 
-export async function uploadCSV(file: File) {
+export async function uploadCSV(file: File): Promise<UploadResponse> {
   const formData = new FormData();
   formData.append("file", file);
 
@@ -14,7 +15,7 @@ export async function uploadCSV(file: File) {
   return res.json();
 }
 
-export async function askQuestion(question: string) {
+export async function askQuestion(question: string): Promise<AskResponse> {
   const res = await fetch(`${API_URL}/ai/ask`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
